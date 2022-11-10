059ee14cb95086f2cf3f8445a42ef64f9bebe442
Benjaneb <benjaneb1@gmail.com>
2022-11-10 18:56:28 +0100

    Whoops, forgot to save

--- sieve/sieve.asm
+++ sieve/sieve.asm
@@ -6,30 +6,17 @@
 #######
 
 .data
-primes:	.space	125	# Allocate 125 bytes as we need 1000 bits (1000/8=125)
-# Every bit will represent a natural number up to 1000, if it is set equal to 1 it is not a prime number. Ex:
-# 123456789
-# 100101011
+primes:	.space	1000	# Allocate 1000 bytes
+# Every byte represents a natural number up to 1000, if it is set equal to 1 it is not a prime number.
 
 .text
 
 main:
-	li	$a0, 69
-	li	$a1, 100
-	li	$a2, 5
-	li	$a3, 127
-	li	$v0, 31
-	syscall
-	li	$a0, 105
-	li	$v0, 32
-	syscall
-	j	main
+	li	$v0, 5
+	syscall		# Read integer (n)
 	
-#	li	$v0, 5
-#	syscall		# Read integer (n)
-	
-#	li	$s0, 2	# Set p=2
-#	la	$s1, primes	# Set s1 to address of primes
+	li	$s0, 2	# Set p=2
+	la	$s1, primes	# Set s1 to address of primes
 	
 	
 	# Exit
