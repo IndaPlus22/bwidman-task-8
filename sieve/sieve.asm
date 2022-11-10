5756318e7596a26dd38708224656d010a01570a9
Benjaneb <benjaneb1@gmail.com>
2022-11-10 18:55:23 +0100

    Change to every number being a byte instead of using a bitmap

--- sieve/sieve.asm
+++ sieve/sieve.asm
@@ -5,15 +5,6 @@
 # Author: Benjamin Widman
 #######
 
-.macro	LOAD_BIT (%rs, %index)
-	lb	%rs, primes
-	
-.end_macro
-
-.macro SET_BIT (%index, %value)
-	
-.end_macro
-
 .data
 primes:	.space	125	# Allocate 125 bytes as we need 1000 bits (1000/8=125)
 # Every bit will represent a natural number up to 1000, if it is set equal to 1 it is not a prime number. Ex:
@@ -23,16 +14,24 @@ primes:	.space	125	# Allocate 125 bytes as we need 1000 bits (1000/8=125)
 .text
 
 main:
-	li	$v0, 5
-	syscall		# Read integer (n)
+	li	$a0, 69
+	li	$a1, 100
+	li	$a2, 5
+	li	$a3, 127
+	li	$v0, 31
+	syscall
+	li	$a0, 105
+	li	$v0, 32
+	syscall
+	j	main
 	
-	li	$s0, 2	# Set p=2
-	la	$s1, primes	# Set s1 to address of primes
+#	li	$v0, 5
+#	syscall		# Read integer (n)
+	
+#	li	$s0, 2	# Set p=2
+#	la	$s1, primes	# Set s1 to address of primes
 	
 	
 	# Exit
 	li	$v0, 10
 	syscall
-	
-load_bit:
-	
